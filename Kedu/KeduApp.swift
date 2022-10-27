7b189c4e1df85882c3f3a81e41e35403730c9e32
Saja Alhimiary <sajaalhimiary@yahoo.com>
2022-10-27 11:27:23 +0300

    Initial Commit

---
+++ Kedu/KeduApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeduApp.swift
+//  Kedu
+//
+//  Created by Saja Alhimiary on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct KeduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
