8d353e6539ae37c4c074698453156d20434cc0a4
Saja Alhimiary <sajaalhimiary@yahoo.com>
2022-11-01 20:03:53 +0300

    watch story video

---
+++ Kedu/Kedu/LetsBeFriends/letsBeFriends_6.swift
@@ -0,0 +1,90 @@
+//
+//  letsBeFriends_6.swift
+//  Kedu
+//
+//  Created by Saja Alhimiary on 30/10/2022.
+//
+
+import SwiftUI
+
+struct letsBeFriends_6: View {
+    @State private var previousIsClicked = false
+    @State private var watchStoryIsClicked = false
+    var body: some View {
+        ZStack {
+            Image("Background")
+            VStack{
+//                HStack{
+//                    Image("Back").resizable()
+//                        .frame(width: 100, height: 100)
+//                        .shadow(radius: 10)
+//                    Image(systemName: "settings")
+//                }
+                    HStack{
+                        Button("〈 ", action: {previousIsClicked = true})
+                            .font(.system(size: 64))
+                            .foregroundColor(.gray)
+                            .shadow(radius: 10)
+                        VStack{
+                            HStack{
+                                Image("21")
+                                    .resizable()
+                                    .frame(width: 450, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .shadow(radius: 20)
+                                    .padding(.leading, 80)
+                                Image("22")
+                                    .resizable()
+                                    .frame(width: 450, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .shadow(radius: 20)
+                                    .padding(.trailing, 180)
+                            }
+                            HStack{
+                                Image("23")
+                                    .resizable()
+                                    .frame(width: 450, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .shadow(radius: 20)
+                                    .padding(.leading, 80)
+                                Image("24")
+                                    .resizable()
+                                    .frame(width: 450, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .shadow(radius: 20)
+                                    .padding(.trailing, 180)
+                            }
+                        }
+                }
+                    .padding()
+                ZStack{
+                    Image("RoundedRectangle").resizable()
+                        .frame(width: 280, height: 80).shadow(radius: 10)
+                        .padding()
+                    HStack{
+                        Image(systemName: "play")
+                            .foregroundColor(.white)
+                            .font(.system(size: 26))
+                        Button("Watch The Story", action: {watchStoryIsClicked.toggle()})
+                            .foregroundColor(.white)
+                            .font(.system(size: 26))
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+            if previousIsClicked{
+                letsBeFriends_5()
+            }
+            else if  watchStoryIsClicked{
+                watchStory()
+            }
+        }
+        .padding()
+    }
+}
+
+struct letsBeFriends_6_Previews: PreviewProvider {
+    static var previews: some View {
+        letsBeFriends_6()
+    }
+}
---
+++ untitled folder/Kedu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Kedu
+//
+//  Created by Saja Alhimiary on 27/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            Image("Background")
+                .padding()
+        }
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
